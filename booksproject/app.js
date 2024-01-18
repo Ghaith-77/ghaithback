7f9789ea3cfb36515ddeaf68d4525e83fb3e3467
Ghaith <gaesx1x2@gmail.com>
2024-01-18 06:33:51 -0800

    s

--- booksproject/app.js
+++ booksproject/app.js
@@ -4,7 +4,7 @@ let app = express();
 let joi = require("joi");
 
 let bookspath = require("./routes/books");
-let authorspath = require("./routes/authors");
+let authorspath = require("./routes/auth");
 
 let mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://gaesx1x2:gaesx1x2@cluster0.t9sqeyn.mongodb.net/?retryWrites=true&w=majority").then(() => {
@@ -17,10 +17,7 @@ app.use(express.json());
 
 app.use("/books", bookspath);
 
-app.use("/authors", authorspath);
-
-app.use(express.json());
-app.use("/books", bookspath);
+app.use("/auth", authorspath);
 
 app.listen(3000, () => {
   console.log("Iam running");
