58c328718504f91f99bfc3fe306da5ef0d80a5da
Ghaith <gaesx1x2@gmail.com>
2024-03-06 06:41:40 -0800

    y hg

--- booksproject/routes/reset_password.js
+++ booksproject/routes/reset_password.js
@@ -2,6 +2,7 @@ let express = require("express");
 let asyncH = require("express-async-handler");
 const { usermodel } = require("../modles/usermodel");
 let jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs/dist/bcrypt");
 let router = express.Router();
 
 router.get(
@@ -12,7 +13,7 @@ router.get(
 );
 router.post(
   "/sendforgotpasswordlink",
-  asyncHa(async(req, res) => {
+  asyncH(async(req, res) => {
     let user = await usermodel.findOne({ email: req.body.email });
     if (!user) {
       return res.status(404).json({ massege: "email rong" });
@@ -42,6 +43,27 @@ router.get(
     }
         
     
+   
+  })
+);
+router.post(
+  "/passwordChangedview",
+   asyncH(async(req, res) => {
+    let user = await usermodel.findById(req.params.userid);
+    if (!user) {
+      return res.status(404).json({ massege: "user not exsist " });
+    }
+    let secretkey = process.env.secretkey + user.password;
+    try{
+        jwt.verify(req.params.token,secretkey)
+        let salt = bcrypt.genSalt(10)
+        req.body.password = bcrypt.hash(req.body.password , salt)
+        user.password =  req.body.password
+        usermodel.save()
+        res.render("passwordChangedview",{email:user.email})
+    }catch (e)  {
+        console.log(e);
+    }
    
   })
 );
