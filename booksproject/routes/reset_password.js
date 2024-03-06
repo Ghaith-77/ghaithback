45c05db0780637be4003511da1854eb9a60194d6
Ghaith <gaesx1x2@gmail.com>
2024-03-06 07:05:02 -0800

    rwe

--- booksproject/routes/reset_password.js
+++ booksproject/routes/reset_password.js
@@ -13,7 +13,8 @@ router.get(
 );
 router.post(
   "/sendforgotpasswordlink",
-  asyncH(async(req, res) => {
+  asyncH(async (req, res) => {
+    console.log(req.body.email);
     let user = await usermodel.findOne({ email: req.body.email });
     if (!user) {
       return res.status(404).json({ massege: "email rong" });
@@ -22,49 +23,45 @@ router.post(
     let token = jwt.sign({ email: user.email, id: user.id }, secretkey, {
       expiresIn: "10m",
     });
-    let link = `http://localhost:3000/reset_password${user.id}/${token}`;
+    let link = `http://localhost:3000/reset_password/forgotpasswordview/${user.id}/${token}`;
     res.json({ massege: "click on link", resetpasswordlink: link });
   })
 );
 router.get(
   "/forgotpasswordview",
-   asyncH(async(req, res) => {
+  asyncH(async (req, res) => {
     let user = await usermodel.findById(req.params.userid);
     console.log(req.params.userid);
     if (!user) {
       return res.status(404).json({ massege: "user not exsist " });
     }
     let secretkey = process.env.secretkey + user.password;
-    try{
-        jwt.verify(req.params.token,secretkey)
-        res.render("reset-password-view",{email:user.email})
-    }catch (e)  {
-        console.log(e);
+    try {
+      jwt.verify(req.params.token, secretkey);
+      res.render("reset-password-view", { email: user.email });
+    } catch (e) {
+      console.log(e);
     }
-        
-    
-   
   })
 );
 router.post(
   "/passwordChangedview",
-   asyncH(async(req, res) => {
+  asyncH(async (req, res) => {
     let user = await usermodel.findById(req.params.userid);
     if (!user) {
       return res.status(404).json({ massege: "user not exsist " });
     }
     let secretkey = process.env.secretkey + user.password;
-    try{
-        jwt.verify(req.params.token,secretkey)
-        let salt = bcrypt.genSalt(10)
-        req.body.password = bcrypt.hash(req.body.password , salt)
-        user.password =  req.body.password
-        usermodel.save()
-        res.render("passwordChangedview",{email:user.email})
-    }catch (e)  {
-        console.log(e);
+    try {
+      jwt.verify(req.params.token, secretkey);
+      let salt = bcrypt.genSalt(10);
+      req.body.password = bcrypt.hash(req.body.password, salt);
+      user.password = req.body.password;
+      usermodel.save();
+      res.render("passwordChangedview");
+    } catch (e) {
+      console.log(e);
     }
-   
   })
 );
 module.exports = router;
